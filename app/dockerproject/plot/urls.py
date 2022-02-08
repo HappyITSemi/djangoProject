d4e335e65546ea0efe78b2279c10b38f38cb6067
happyITsemi <65163342+HappyITSemi@users.noreply.github.com>
2022-02-08 12:39:18 +0900

    move 1

--- app/dockerproject/plot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from app.dockerproject.plot import views
-
-app_name = 'plot'
-urlpatterns = [
-    path('', views.PlotIndexView.as_view(), name='plot_index'),
-    # path('show/', views.PlotShowView.as_view(), name='plot_show'),
-]
--- app/dockerproject/todo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from todo.models import Todo, Category
-
-admin.site.register(Todo)
-admin.site.register(Category)
