ec6dfa58e78823e67088cc6cf270094409b011ee
happyITsemi <65163342+HappyITSemi@users.noreply.github.com>
2022-03-05 15:21:57 +0900

    lambda-1

---
+++ lambda-function/lambda-dynamo/read_dynamodb_event/app.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        print('EventID: ' + record['eventID'])
+        print('EventName: ' + record['eventName'])
+    print('Successfully processed %s records.' % str(len(event['Records'])))
